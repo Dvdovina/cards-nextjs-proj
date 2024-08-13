d3bf420eddec9c4389dbf02fe217ee549965a6f7
Daria Vdovina <vdovinadn@gmail.com>
2024-08-13 19:04:12 +0300

    build: redux setup, depend., gitignore

---
+++ src/redux/apiSlice.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.example.com' }), 
+  endpoints: (builder) => ({
+    getItems: builder.query({
+      query: () => '/endpoint',
+    }),
+  }),
+});
+
+export const { useGetItemsQuery } = apiSlice;
